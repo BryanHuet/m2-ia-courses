4f6a583422842a90ddfc530cbbf8d4be18d8a9e1
BryanHuet <huetb050@gmail.com>
2022-10-13 21:30:07 +0200

    update bellman

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.*;
 import bw.*;
 import bw.maps.*;
 import bw.mdp.*;
@@ -10,36 +11,47 @@ import simulation.*;
 import toyproblems.*;
 import visualization.*;
 
-
 public class Main{
     public static void main(String args[])
-    
+
     {
         Random rand = new Random();
 
         // Problem definition
         System.out.print("Building problem...");
         System.out.flush();
+
+
         //BWMap map = new SeveralBridgesBWMap();
         BWMap map = new IslandsBWMap();
         int maxSpeed = 3;
         BWMDP mdp = BWMDP.makeStandardProblem(map, maxSpeed);
         System.out.println(" done.");
 
+
         // Policy computation
         System.out.print("Building policy...");
         System.out.flush();
+
+
+
         //StationaryPolicy<BWState, BWAction> policy = new UniformMixedPolicy<>(mdp, rand);
-        AdHocIslandsPolicy policy = new AdHocIslandsPolicy(map);
-        
+        //AdHocIslandsPolicy policy = new AdHocIslandsPolicy(map);
+
+        double gamma = 0.9;
+        double epsilon = 0.1;
+        ValueIterationSolver<BWState, BWAction> solver = new ValueIterationSolver<>(mdp, gamma, epsilon);
+        StationaryPolicy<BWState, BWAction> policy = solver.buildPolicy();
+
+
         System.out.println(" done.");
 
-        // Agent definition, at a random initial state      
+        // Agent definition, at a random initial state
         int row = rand.nextInt(map.height);
         int col = rand.nextInt(map.width);
         BWState initialState = mdp.stateFactory.getState(row, col, 0);
         Agent<BWState> agent = new Agent<>(initialState);
-        System.out.println("Agent created at initial state " + initialState);       
+        System.out.println("Agent created at initial state " + initialState);
 
         // Simulation
         System.out.println("Running simulation forever...");
@@ -47,38 +59,39 @@ public class Main{
         simu.addAgent(agent, new PolicyBasedAgentController<>(policy, initialState));
         int sleep = 200;
         simu.run(sleep);
-        
+
 
     }
 
 
-/* 
+/*
     {
-        Random rand = new Random();
+        double value = 5;
+        double gamma = 0.9;
+        double epsilon = 0.1;
+        TinyMDP tinyMDP = new TinyMDP(value);
 
-        // Problem definition
-        System.out.print("Building problem...");
-        System.out.flush();
-        BWMap map = new SeveralBridgesBWMap();
-        int maxSpeed = 3;
-        BWMDP mdp = BWMDP.makeStandardProblem(map, maxSpeed);
-        System.out.println(" done.");
+        ValueIterationSolver<String, String> solver = new ValueIterationSolver<>(tinyMDP, gamma, epsilon);
+        StationaryPolicy<String, String> policy = solver.buildPolicy();
 
-        // Policy computation
-        System.out.print("Building policy...");
-        System.out.flush();
-        //StationaryPolicy<BWState, BWAction> policy = new UniformMixedPolicy<>(mdp, rand);
-        AdHocIslandsPolicy policy = new AdHocIslandsPolicy(map);
-        System.out.println(" done.");
-
-        // Policy visualization
-        for (int speed = 0; speed <= maxSpeed; speed++) {
-            System.out.print("Displaying policy at speed " + speed);
-            System.out.flush();
-            BWPolicyViewer viewer = new BWPolicyViewer(mdp, policy, speed);
-            viewer.showFrame("Bridge world policy at speed " + speed);
-            System.out.println(" done.");
+        boolean ok = true;
+        for (String state : tinyMDP.states()) {
+            Set<String> optimalActions = tinyMDP.optimalActions(state, gamma);
+            String computedAction = policy.get(state);
+            if (!optimalActions.contains(computedAction)) {
+                System.out.println("ERROR: VISolver computed action " + computedAction);
+                System.out.println(
+                   "Problem is TinyMDP with value " + value + ", gamma is " + gamma + ", epsilon is " + epsilon);
+                System.out.println("True set of optimal actions is " + optimalActions);
+                ok = false;
+                break;
+            }
         }
+
+        if (ok) {
+            System.out.println("Test passed");
+        }
+
 
     }
 */
--- ValueIterationSolver.java
+++ ValueIterationSolver.java
@@ -1,3 +1,5 @@
+
+import java.util.*;
 import control.*;
 import mdp.*;
 import bw.mdp.BWAction;
@@ -13,40 +15,78 @@ public class ValueIterationSolver<S, A>{
         this.mdp = mdp;
         this.epsilon = epsilon;
         this.gamma = gamma;
-    
-    }
 
-    public Double backup1(S initialState){
-        double currentMax = -10000.1;
-        double som = 0.0;
-        for(A action: this.mdp.actions(initialState)){
-            for (S nextState: this.mdp.getTransitionFunction().getNextStatesDistribution(initialState,action).support()){
-                som += this.mdp.getTransitionFunction().getTransitionProbability(initialState,action,nextState)
-                    * ((this.mdp.getRewardFunction().getReward(initialState,action,nextState)
-                        + gamma * this.backup1(nextState)));
+    }
+    /*
+        public Double backup1(S initialState){
+            double currentMax = -10000.1;
+            double som = 0.0;
+
+            for(A action: this.mdp.actions(initialState)){
+                for (S nextState: this.mdp.getTransitionFunction().getNextStatesDistribution(initialState,action).support()){
+                    som += this.mdp.getTransitionFunction().getTransitionProbability(initialState,action,nextState)
+                        * ((this.mdp.getRewardFunction().getReward(initialState,action,nextState)
+                            + gamma * this.backup1(nextState)));
+                }
+
+
+                if (som >= currentMax){
+                    currentMax=som;
+                }
             }
-            if (som >= currentMax){
-                currentMax=som;
+
+            return currentMax;
+        }
+    */
+    public HashMap<A, Double> backup(HashMap<A, Double> initialReward, S initialState, A action){
+
+        HashMap<A, Double> rewards = new HashMap<>();
+        double som = 0.0;
+
+        if (initialReward.containsKey(action)){
+            som += initialReward.get(action);
+        } else {
+
+            for (S nextState: this.mdp.getTransitionFunction().getNextStatesDistribution(initialState,action).support()){
+                rewards.put(action,som);
+                som += this.mdp.getTransitionFunction().getTransitionProbability(initialState,action,nextState)
+                        * ((this.mdp.getRewardFunction().getReward(initialState,action,nextState)
+                        + gamma * backup(rewards,nextState,action).get(action)));
             }
         }
 
-        return currentMax;
-    }
+        rewards.put(action,som);
 
-    public RewardFunction<S,A> backup (RewardFunction<A,S> initialReward){
-        return new RewardFunction<S, A>() {
-            @Override
-            public double getReward(S s, A a, S s1) {
-                return 0;
-            }
-        };
+        return rewards;
     }
 
     public StationaryPolicy<S,A> buildPolicy(){
         return new StationaryPolicy<S, A>() {
             @Override
             public A get(S s) {
-                return null;
+                double maxReward = -10000.1;
+                A bestAction = null;
+                HashMap<A, Double> rewards = new HashMap<>();
+
+                for(A action: mdp.actions(s)){
+
+                    rewards = backup(rewards, s, action);
+
+
+
+                }
+
+                for (Map.Entry<A, Double> entry : rewards.entrySet()) {
+                    if(entry.getValue()> maxReward){
+                        maxReward = entry.getValue();
+                        bestAction=entry.getKey();
+                    }
+                }
+
+
+
+
+                return bestAction;
             }
         };
     }
